b6bc55377f77274059664a3a9e099110426c7d0c
vimlesh88d <kumarvimlesh.it@gmail.com>
2020-10-15 13:31:51 +0530

    modified web.xml and controller

--- src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
+++ src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
@@ -1,17 +1,16 @@
 
 package org.arpit.java2blog.springmvc.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HelloWorldController {
- 
- @RequestMapping("/helloworld")
- public String hello() {
- 
-return "Heloo New World ";
-}}
 
-
+	@RequestMapping("/helloworld")
+	public ModelAndView hello() {
+		String helloWorldMessage = "Hello new world from java2blog1";
+		return new ModelAndView("hello", "message", helloWorldMessage);
+	}
+}
